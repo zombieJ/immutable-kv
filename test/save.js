2f7cc5be13a109b7fb1543ef9cc5e22689396494
jiljiang <jiljiang@ebay.com>
2017-04-30 02:28:10 +0800

    test case

--- test/save.js
+++ test/save.js
@@ -1,9 +1,13 @@
 import { assert, deepEqual } from 'chai';
-import { KV, KVFileInfo } from '../js/index';
+import { KV } from '../js/index';
 
+const FS = require('fs');
 const PATH = require('path');
 
 describe.only('KVFileInfo Save Test', () => {
+	const DEL = require('del');
+	DEL.sync(PATH.resolve(__dirname, 'out'));
+
 	it('save', () => {
 		const targetPath = PATH.resolve(__dirname, 'out/kv.txt');
 
@@ -11,6 +15,12 @@ describe.only('KVFileInfo Save Test', () => {
 			.baseLoad(PATH.resolve(__dirname, 'res/kv_base.txt'))
 			.then((info) => {
 				return info.save(targetPath);
+			})
+			.then(() => {
+				assert.isTrue(FS.existsSync(PATH.resolve(__dirname, 'out')));
+				assert.isTrue(FS.existsSync(PATH.resolve(__dirname, 'out/kv.txt')));
+				assert.isTrue(FS.existsSync(PATH.resolve(__dirname, 'out/next/kv1.txt')));
+				assert.isTrue(FS.existsSync(PATH.resolve(__dirname, 'out/next/kv2.txt')));
 			});
 
 		return promise;
